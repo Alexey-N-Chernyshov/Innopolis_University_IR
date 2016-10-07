6447426891d4e4182f260fe7db66a71355545024
Alexey Chernyshov <Alexey.N.Chernyshov@gmail.com>
2016-10-07 21:24:41 +0300

    add retrieval app

--- general/src/com/github/alexey_n_chernyshov/iu/ir/boolean_retrieval/BooleanRetrievalApp.scala
+++ general/src/com/github/alexey_n_chernyshov/iu/ir/boolean_retrieval/BooleanRetrievalApp.scala
@@ -4,26 +4,15 @@
 
 package com.github.alexey_n_chernyshov.iu.ir.boolean_retrieval
 
+import com.github.alexey_n_chernyshov.iu.ir.{Retrieval, RetrievalApp}
+
 import scala.io.StdIn.readLine
 
-object BooleanRetrievalApp {
+object BooleanRetrievalApp extends RetrievalApp {
 
-  def main(args: Array[String]): Unit = {
-    val corpus = "data/ex1"
-    println("Boolean retrieval.")
-    println("Building corpus from " + corpus)
-    val booleanRetrieval = new BooleanRetrieval(corpus)
+  val corpus = "data/ex1"
 
-    while (true) {
-      print("\nInput query: ")
-      val query = readLine()
-      println("Result:")
-      try {
-        booleanRetrieval.executeQuery(query).foreach(println(_))
-      } catch {
-        case e: UnsupportedOperationException => println("Wrong query: " + e)
-      }
-    }
-  }
+  override val retrievalName = "Boolean retrieval"
+  override val retrievalModel = new BooleanRetrieval(corpus)
 
 }
