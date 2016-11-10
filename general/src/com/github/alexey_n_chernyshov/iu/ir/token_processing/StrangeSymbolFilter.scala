07441b1cc3e1ee71c8a41f745ef6e7c1636bf089
Alexey Chernyshov <Alexey.N.Chernyshov@gmail.com>
2016-11-10 15:51:49 +0300

    fix filter ASCII letters

--- general/src/com/github/alexey_n_chernyshov/iu/ir/token_processing/StrangeSymbolFilter.scala
+++ general/src/com/github/alexey_n_chernyshov/iu/ir/token_processing/StrangeSymbolFilter.scala
@@ -4,17 +4,15 @@
 
 package com.github.alexey_n_chernyshov.iu.ir.token_processing
 
-import scala.util.Try
-
 class StrangeSymbolFilter(parentTokenFilter: TokenFilter) extends TokenFilter {
 
   /**
     *
     * @param token - token itself
-    * @return true if token doesn't contain symbols
+    * @return true if token consists of ASCII letters
     */
   override def isOk(token: String): Boolean = {
-    token.forall(_.isLetter) && parentTokenFilter.isOk(token)
+    token.forall(c => c.isLetter && (c <= 'z')) && parentTokenFilter.isOk(token)
   }
 
 }
